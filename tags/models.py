87b52b4edb4c7d555bda9baea92bd034ee1a0827
Wubeshet Yimam <w.yimam@alustudent.com>
2023-05-10 00:56:22 +0300

    Add Tag and TaggedItem

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+    
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # the following field is a generic foreign key, which means it can reference any model.
+    # the content_type field is a ForeignKey to the ContentType model, 
+    # which is a system model that stores information about all of the models in your Django project.
+    # the object_id field is an IntegerField that stores the primary key of the related object.
+    # the content_object field is a GenericForeignKey, which is a field that 
+    # can point to any object, based on the content_type and object_id fields.
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    # the following line is a GenericForeignKey, which is a field that can point
+    # to any object, based on the content_type and object_id fields.
+    content_object = GenericForeignKey()
