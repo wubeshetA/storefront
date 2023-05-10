cb27660d9b983852c61e48c5b4943e4bad00e428
Wubeshet Yimam <w.yimam@alustudent.com>
2023-05-10 04:12:20 +0300

    Add mysql to project

--- storefront/settings.py
+++ storefront/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,8 +88,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        # 'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': os.getenv("DB_PASSWORD"),
     }
 }
 
