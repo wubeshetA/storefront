1ea7c29845937f11e013ae3d1f1c33147ab0efc6
Wubeshet Yimam <w.yimam@alustudent.com>
2023-05-10 23:48:19 +0300

    Add solutions to exercise on codingwithMosh course

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
+from store.models import Customer
+from store.models import Collection
+from store.models import Order
+from store.models import OrderItem
 
 # Create your views here.
 def hello_world(request):
+    
+    # query_set = Product.objects.all()
+    # for item in query_set:
+    #     # print(item)
+    #     pass
+    # =================================
+    # exercise 1
+    # customers = Customer.objects.filter(email__endswith=".com")
+    # for customer in customers:
+    #     print(customer, " " ,customer.email)
+        
+    # ==========================================
+    # exercise 2
+    # collections = Collection.objects.filter(featured_product__isnull=True)
+    # for collection in collections:
+    #     print(collection, " " ,collection.featured_product)
+    
+    # =================================================
+    # exercise 3
+    # products = Product.objects.filter(inventory__lt=10)
+    # for product in products:
+    #     print(product.title, " " ,product.inventory)
+    
+    # =========================================
+    # exercise 4
+    # orders = Order.objects.filter(customer=6)
+    # for order in orders:
+    #     print(order.id, " " ,order.customer)
+    # =====================================================
+    
+    # exercise 5
+    # query_set = Product.objects.all()
+    # order_items = OrderItem.objects.filter(product__collection__id=3)
+    # for item in order_items:
+    #     print(item.id, " " ,item.product.title, item.product.collection.pk, item.product.collection.title)
+    
     return render(request, 'hello.html', {'name': 'Wube'})
