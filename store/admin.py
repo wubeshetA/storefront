813d454caaffc49cab55f85da344070da519c575
Wubeshet Yimam <w.yimam@alustudent.com>
2023-05-15 00:32:28 +0300

    Add link to customer & collection tables in admin

--- store/admin.py
+++ store/admin.py
@@ -5,13 +5,14 @@ from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
 from . import models
 from django.db.models.aggregates import Count
-from django.utils.html import format_html
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 
 # Register your models here.
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('title', 'unit_price', 'inventory_status')
+    list_display = ('title', 'unit_price', 'collection', 'inventory_status')
     list_editable = ('unit_price',)
     list_per_page = 10
     
@@ -23,9 +24,31 @@ class ProductAdmin(admin.ModelAdmin):
     
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'membership')
+    list_display = ('first_name', 'last_name', 'membership', 'orders_count')
     list_editable = ('membership',)
-    list_per_page = 10
+ 
+    
+    @admin.display(ordering='orders_count')
+    def orders_count(self, customer):
+        # the following reverse method is used to get the url of the admin page we want to redirect to
+        # here is the format of the reverse method
+        # reverse(viewname, urlconf=None, args=None, kwargs=None, current_app=None)
+        #  or 
+        # reverse("admin:app_model_page") e.g reverse("admin:store_order_changelist")
+        url = (
+            reverse("admin:store_order_changelist")
+                       + "?" 
+                       + urlencode(
+                           {"customer__id": str(customer.id)}
+                           ))
+        return format_html('<a href="{}">{}</a>', url, customer.orders_count)
+    
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+            return super().get_queryset(request).annotate(
+                orders_count=Count('order')
+            )
+    
 
 
 @admin.register(models.Order)
@@ -36,13 +59,24 @@ class OrderAdmin(admin.ModelAdmin):
 
     
 @admin.register(models.Collection)
-
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ('title', 'products_count')
     
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
+        # the following reverse method is used to get the url of the admin page we want to redirect to
+        # here is the format of the reverse method
+        # reverse(viewname, urlconf=None, args=None, kwargs=None, current_app=None)
+        #  or 
+        # reverse("admin:app_model_page")
+        url = (
+            reverse("admin:store_product_changelist")
+                       + "?" 
+                       + urlencode(
+                           {"collection__id": str(collection.id)}
+                           ))
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
+       
     
     def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
         return super().get_queryset(request).annotate(
