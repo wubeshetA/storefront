250579727f039171e513b190e0fdd57cf7c634dc
Wubeshet Yimam <w.yimam@alustudent.com>
2023-05-18 15:50:25 +0300

    create custome serializer field

--- store/views.py
+++ store/views.py
@@ -5,14 +5,14 @@ from .models import Product
 from .serializer import ProductSerializer
 
 print("welcome")
-@api_view(['GET'])
+@api_view()
 def products_list(request):
 
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
-@api_view
+@api_view()
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
