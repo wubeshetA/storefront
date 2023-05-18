b16e448ec69b466f37bf1ca1596e2adcee66da29
Wubeshet Yimam <w.yimam@alustudent.com>
2023-05-18 16:28:48 +0300

    Replace Serializer base calls with ModelSerializer

--- store/serializer.py
+++ store/serializer.py
@@ -3,17 +3,39 @@ from rest_framework import serializers
 from .models import Product
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-class ProductSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=225)
-    description = serializers.CharField(min_length=2, max_length=225)
-    price = serializers.DecimalField(
-        max_digits=6,
-        decimal_places=2,
-        source='unit_price'
-    )
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title']
+        
+        
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+class ProductSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Product
+        fields = [
+
+            'title',
+            'description',
+            'unit_price',
+            'price_with_tax',
+            'collection',
+        
+        ]
+        
+    """The following fields were added when the serializer was created
+    with the Serializer class not ModelSerializer. Now since we have 
+    ModelSerializer we don't need them anymore."""
+    
+    # title = serializers.CharField(max_length=225)
+    # description = serializers.CharField(min_length=2, max_length=225)
+    # price = serializers.DecimalField(
+    #     max_digits=6,
+    #     decimal_places=2,
+    #     source='unit_price'
+    # )
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     
     # Adding RelatedField by PrimaryKeyField
