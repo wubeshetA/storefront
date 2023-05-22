7d94c0ba8ac54cc67b46c3f18f9cff3b66d156bd
Wubeshet Yimam <w.yimam@alustudent.com>
2023-05-23 00:26:36 +0300

    Eager load product

--- store/views.py
+++ store/views.py
@@ -330,14 +330,18 @@ class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
     
     
-class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
+class CartViewSet(CreateModelMixin,
+                  RetrieveModelMixin,
+                  DestroyModelMixin,
+                  GenericViewSet):
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
-    
+
 class CartItemViewSet(ModelViewSet):
     
     def get_queryset(self):
-        return CartItem.objects.filter(cart_id=self.kwargs.get('cart_pk'))
+        return CartItem.objects.filter(cart_id=self.kwargs.get('cart_pk'))\
+            .select_related('product')
     
     def get_serializer_context(self):
         return {'cart_id': self.kwargs.get('cart_pk')}
