b9de26ecc5f0ddb736f94e0920516116864ae53d
Wubeshet Yimam <w.yimam@alustudent.com>
2023-05-25 02:30:08 +0300

    Get customer profile with customer/me endpoint

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 # import Count class
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -370,12 +370,23 @@ class CartItemViewSet(ModelViewSet):
     
     def get_serializer_context(self):
         return {'cart_id': self.kwargs.get('cart_pk')}
-    
-    
+
 class CustomerViewSet(CreateModelMixin,
                       RetrieveModelMixin, 
                       UpdateModelMixin,
                       GenericViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+    @action(detail=False, methods=['GET', 'PUT'])
+    def me(self, request):
+        (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
 
