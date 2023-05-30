682d3a65741eca1549bbef8bb3f5ae0dfc395a4f
Wubeshet Yimam <w.yimam@alustudent.com>
2023-05-30 16:47:05 +0300

    Add fixtures

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,34 +1,39 @@
-from rest_framework.test import APIClient
 from rest_framework import status
 import pytest
 from django.contrib.auth.models import User
 
+
+@pytest.fixture
+def create_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post('/store/collections/', collection)
+    return do_create_collection
+
+
 @pytest.mark.django_db
 class TestCreateCollection:
-    def test_if_user_is_anonymous_returns_401(self):
+    def test_if_user_is_anonymous_returns_401(self, create_collection):
         # Arrange
         # Act
-        client = APIClient()
-        response = client.post('/store/collections/', {'title': 'a'})
+        response = create_collection({'title': 'a'})
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    
-    def test_if_user_is_not_admin_returns_403(self):
-        client = APIClient()
-        client.force_authenticate(user={})
-        response = client.post('/store/collections/', {'title': 'a'})
-        assert response.status_code == status.HTTP_403_FORBIDDEN
-    
-    def test_if_data_is_not_valid_returns_400(self):
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post('/store/collections/', {'title': ''})
+
+    def test_if_user_is_not_admin_returns_403(self, authenticate, create_collection):
+
+        authenticate(is_staff=False) # Arrage 
+        response = create_collection({'title': 'a'}) # Act
+        assert response.status_code == status.HTTP_403_FORBIDDEN # Assert
+
+    def test_if_data_is_not_valid_returns_400(self, authenticate, create_collection):
+        authenticate(is_staff=True)
+        response = create_collection({'title': ''})
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data.get('title') is not None
-    
-    def test_if_data_is_valid_returns_201(self):
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post('/store/collections/', {'title': 'a'})
+
+    def test_if_data_is_valid_returns_201(self, authenticate, create_collection):
+
+        authenticate(is_staff=True)
+        response = create_collection({'title': 'a'})
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data.get('id') > 0
