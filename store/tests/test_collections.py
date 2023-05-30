833899295c5f7c88ae4308e19391d85ab256d49d
Wubeshet Yimam <w.yimam@alustudent.com>
2023-05-30 19:34:16 +0300

    Add tests for listing and updating collections

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -63,3 +63,50 @@ class TestRetrieveCollection:
         response = api_client.get(f'/store/collections/{collection.id}/')
         assert response.status_code == status.HTTP_200_OK
         assert response.data.get('products_count') == 3
+        
+
+@pytest.mark.django_db
+class TestListCollection:
+    
+    def test_if_collections_exist_returns_200(self, api_client):
+        baker.make(Collection, _quantity=3)
+        response = api_client.get(f'/store/collections/')
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.data) == 3
+    
+@pytest.mark.django_db
+class TestUpdateCollection:
+    def test_if_user_is_anonymous_returns_401(self, api_client):
+        collection = baker.make(Collection)
+        response = api_client.put(f'/store/collections/{collection.id}/', {'title': 'a'})
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    
+    def test_if_user_is_not_admin_returns_403(self, authenticate, api_client):
+        collection = baker.make(Collection)
+        authenticate(is_staff=False)
+        response = api_client.put(f'/store/collections/{collection.id}/', {'title': 'a'})
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+    
+    def test_if_data_is_not_valid_returns_400(self, authenticate, api_client):
+        collection = baker.make(Collection)
+        authenticate(is_staff=True)
+        response = api_client.put(f'/store/collections/{collection.id}/', {'title': ''})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data.get('title') is not None
+    
+    def test_if_collection_not_exist_returns_404(self, authenticate, api_client):
+        authenticate(is_staff=True)
+        response = api_client.put(f'/store/collections/1/', {'title': 'a'})
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        
+    def test_if_data_is_valid_returns_200(self, authenticate, api_client):
+        collection = baker.make(Collection)
+        authenticate(is_staff=True)
+        response = api_client.put(f'/store/collections/{collection.id}/', {'title': 'a'})
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data.get('title') == 'a'
+        
+# @pytest.mark.django_db
+# class TestDeleteCollection:
+    
+    
