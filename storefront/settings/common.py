7a77da8406e090c8babca7f11fc8c25539da7419
Wubeshet Yimam <w.yimam@alustudent.com>
2023-06-02 23:59:51 +0300

    update base_dir in settings

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -93,7 +93,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'storefront.wsgi.application'
-
 
 
 # Password validation
