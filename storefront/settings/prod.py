56123246d3561daa0b598af12d98a22bce08073c
Wubeshet Yimam <w.yimam@alustudent.com>
2023-06-04 23:28:01 +0300

    Add dj-database-url for parsing Database url

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from .common import *
 
 
@@ -6,3 +7,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['wube-storefront.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config() # this function look for env variable 
+    # called DATABASE_URL and parse it to connection string
+}
