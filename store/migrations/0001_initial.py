483fed338c85c3c81917ccaf43b954e66e6cdbf8
Wubeshet Yimam <w.yimam@alustudent.com>
2023-06-05 19:59:45 +0300

    update migration file manually

--- store/migrations/0001_initial.py
+++ store/migrations/0001_initial.py
@@ -24,7 +24,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Collection',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, editable=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=255)),
             ],
         ),
--- store/migrations/0010_alter_cart_id.py
+++ store/migrations/0010_alter_cart_id.py
@@ -11,9 +11,9 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='cart',
-            name='id',
-            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False),
-        ),
+        # migrations.AlterField(
+        #     model_name='cart',
+        #     name='id',
+        #     field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False),
+        # ),
     ]
